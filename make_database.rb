84aeae675a6ebd3cfb2fd0cd48041f6c3c15b9fd
Will Pearse <will.pearse@gmail.com>
2014-10-13 15:06:36 -0500

    One more time on the master

--- make_database.rb
+++ make_database.rb
@@ -31,7 +31,7 @@ puts "*** Renamed some microclimates files to add .csv and remove .dxd and give 
 puts "*** Only loads the processed soil microclimate data; the raw structure is intelligible but I'm unwilling to put my neck out and guess!"
 puts "Each full stop = one city's data loaded"
 
-if ARGV.length == 3
+if ARGV.length == 4
   if File.exist? ARGV[0]
     puts "\nERROR: Cowardly refusing to output to an existing database"
   else
@@ -57,46 +57,48 @@ if ARGV.length == 3
     ########################
     #Soil microclimate######
     ########################
-    print "\nLoading soil microclimate data  ";$stdout.flush
-    soil_microclimate = DataFrame.new({:date=>[],:city_parcel=>[],:moisture=>[],:temperature=>[]})
-    #Baltimore
-    Dir.foreach("Soil Moisture Sensor Data/Baltimore") {|file| if file[".xls"] then soil_microclimate << read_soil_microclimate("Soil Moisture Sensor Data/Baltimore/"+file, "BA", 2) end}
-    
-    Dir.foreach("Soil Moisture Sensor Data/Baltimore/Forested Sites with name corrections") {|file| if file[".xls"] then soil_microclimate << read_soil_microclimate("Soil Moisture Sensor Data/Baltimore/Forested Sites with name corrections/"+file, "BA", 0) end}
-    print ".";$stdout.flush
-    #Boston
-    Dir.foreach("Soil Moisture Sensor Data/Boston/Sep") {|file| if file[".xls"] then soil_microclimate << read_soil_microclimate("Soil Moisture Sensor Data/Boston/Sep/"+file, "BOS", 0) end}
-    print ".";$stdout.flush
+    if ARGV[4] == "1"
+      print "\nLoading soil microclimate data  ";$stdout.flush
+      soil_microclimate = DataFrame.new({:date=>[],:city_parcel=>[],:moisture=>[],:temperature=>[]})
+      #Baltimore
+      Dir.foreach("Soil Moisture Sensor Data/Baltimore") {|file| if file[".xls"] then soil_microclimate << read_soil_microclimate("Soil Moisture Sensor Data/Baltimore/"+file, "BA", 2) end}
+      
+      Dir.foreach("Soil Moisture Sensor Data/Baltimore/Forested Sites with name corrections") {|file| if file[".xls"] then soil_microclimate << read_soil_microclimate("Soil Moisture Sensor Data/Baltimore/Forested Sites with name corrections/"+file, "BA", 0) end}
+      print ".";$stdout.flush
+      #Boston
+      Dir.foreach("Soil Moisture Sensor Data/Boston/Sep") {|file| if file[".xls"] then soil_microclimate << read_soil_microclimate("Soil Moisture Sensor Data/Boston/Sep/"+file, "BOS", 0) end}
+      print ".";$stdout.flush
     #Miami
-    Dir.foreach("Soil Moisture Sensor Data/Miami") {|file| if file[".xls"] then soil_microclimate << read_soil_microclimate("Soil Moisture Sensor Data/Miami/"+file, "BOS", 2) end}
+      Dir.foreach("Soil Moisture Sensor Data/Miami") {|file| if file[".xls"] then soil_microclimate << read_soil_microclimate("Soil Moisture Sensor Data/Miami/"+file, "BOS", 2) end}
+      print ".";$stdout.flush
+      
+      ########################
+      #Microclimate###########  
+      ########################
+      print "\nLoading microclimate data       ";$stdout.flush
+      #Baltimore
+      microclimate = DataFrame.new({:city_parcel=>[],:date=>[],:time=>[],:type=>[],:value=>[]})
+      Dir.foreach("iButton Data/Baltimore/August") {|file| if File.file? "iButton Data/Baltimore/August/"+file then microclimate << read_iButton("iButton Data/Baltimore/August/"+file, "BA") end}
+      Dir.foreach("iButton Data/Baltimore/Dec13") {|file| if File.file? "iButton Data/Baltimore/Dec13/"+file then microclimate << read_iButton("iButton Data/Baltimore/Dec13/"+file, "BA") end}
+      Dir.foreach("iButton Data/Baltimore/Forested Sites with Name Correction") {|file| if File.file? "iButton Data/Baltimore/Forested Sites with Name Correction/"+file then microclimate << read_iButton("iButton Data/Baltimore/Forested Sites with Name Correction/"+file, "BA") end}
+      print ".";$stdout.flush
+      #Boston
+      Dir.foreach("iButton Data/Boston/Jan 2014") {|file| if File.file? "iButton Data/Boston/Jan 2014/"+file then microclimate << read_iButton("iButton Data/Boston/Jan 2014/"+file, "BOS") end}
+      Dir.foreach("iButton Data/Boston/Nov 2013") {|file| if File.file? "iButton Data/Boston/Nov 2013/"+file then microclimate << read_iButton("iButton Data/Boston/Nov 2013/"+file, "BOS") end}
+      Dir.foreach("iButton Data/Boston/Sep") {|file| if File.file? "iButton Data/Boston/Sep/"+file then microclimate << read_iButton("iButton Data/Boston/Sep/"+file, "BOS") end}
     print ".";$stdout.flush
-    
-    ########################
-    #Microclimate###########  
-    ########################
-    print "\nLoading microclimate data       ";$stdout.flush
-    #Baltimore
-    microclimate = DataFrame.new({:city_parcel=>[],:date=>[],:time=>[],:type=>[],:value=>[]})
-    Dir.foreach("iButton Data/Baltimore/August") {|file| if File.file? "iButton Data/Baltimore/August/"+file then microclimate << read_iButton("iButton Data/Baltimore/August/"+file, "BA") end}
-    Dir.foreach("iButton Data/Baltimore/Dec13") {|file| if File.file? "iButton Data/Baltimore/Dec13/"+file then microclimate << read_iButton("iButton Data/Baltimore/Dec13/"+file, "BA") end}
-    Dir.foreach("iButton Data/Baltimore/Forested Sites with Name Correction") {|file| if File.file? "iButton Data/Baltimore/Forested Sites with Name Correction/"+file then microclimate << read_iButton("iButton Data/Baltimore/Forested Sites with Name Correction/"+file, "BA") end}
-    print ".";$stdout.flush
-    #Boston
-    Dir.foreach("iButton Data/Boston/Jan 2014") {|file| if File.file? "iButton Data/Boston/Jan 2014/"+file then microclimate << read_iButton("iButton Data/Boston/Jan 2014/"+file, "BOS") end}
-    Dir.foreach("iButton Data/Boston/Nov 2013") {|file| if File.file? "iButton Data/Boston/Nov 2013/"+file then microclimate << read_iButton("iButton Data/Boston/Nov 2013/"+file, "BOS") end}
-    Dir.foreach("iButton Data/Boston/Sep") {|file| if File.file? "iButton Data/Boston/Sep/"+file then microclimate << read_iButton("iButton Data/Boston/Sep/"+file, "BOS") end}
-    print ".";$stdout.flush
-    #Los Angeles
-    Dir.foreach("iButton Data/Los Angeles/Sep") {|file| if File.file? "iButton Data/Los Angeles/Sep/"+file then microclimate << read_iButton("iButton Data/Los Angeles/Sep/"+file, "LA") end}
-    Dir.foreach("iButton Data/Los Angeles/Dec2013Jan2014") {|file| if File.file? "iButton Data/Los Angeles/Dec2013Jan2014/"+file then microclimate << read_iButton("iButton Data/Los Angeles/Dec2013Jan2014/"+file, "LA") end}
-    print ".";$stdout.flush
+      #Los Angeles
+      Dir.foreach("iButton Data/Los Angeles/Sep") {|file| if File.file? "iButton Data/Los Angeles/Sep/"+file then microclimate << read_iButton("iButton Data/Los Angeles/Sep/"+file, "LA") end}
+      Dir.foreach("iButton Data/Los Angeles/Dec2013Jan2014") {|file| if File.file? "iButton Data/Los Angeles/Dec2013Jan2014/"+file then microclimate << read_iButton("iButton Data/Los Angeles/Dec2013Jan2014/"+file, "LA") end}
+      print ".";$stdout.flush
     #Miami
-    Dir.foreach("iButton Data/Miami/July") {|file| if File.file? "iButton Data/Miami/July/"+file then microclimate << read_iButton("iButton Data/Miami/July/"+file, "FL") end}
-    Dir.foreach("iButton Data/Miami/October") {|file| if File.file? "iButton Data/Miami/October/"+file then microclimate << read_iButton("iButton Data/Miami/October/"+file, "FL") end}
-    print ".";$stdout.flush
+      Dir.foreach("iButton Data/Miami/July") {|file| if File.file? "iButton Data/Miami/July/"+file then microclimate << read_iButton("iButton Data/Miami/July/"+file, "FL") end}
+      Dir.foreach("iButton Data/Miami/October") {|file| if File.file? "iButton Data/Miami/October/"+file then microclimate << read_iButton("iButton Data/Miami/October/"+file, "FL") end}
+      print ".";$stdout.flush
     #Minnesota
-    Dir.foreach("iButton Data/MSP") {|file| if File.file? "iButton Data/MSP/"+file then microclimate << read_iButton("iButton Data/MSP/"+file, "MN") end}
-    print ".";$stdout.flush
+      Dir.foreach("iButton Data/MSP") {|file| if File.file? "iButton Data/MSP/"+file then microclimate << read_iButton("iButton Data/MSP/"+file, "MN") end}
+      print ".";$stdout.flush
+    end
     
     ########################
     #iTree##################
@@ -241,13 +243,20 @@ if ARGV.length == 3
     lawn_survey = clean_strings(lawn_survey)
     phone_survey = clean_strings(phone_survey)
     soil = clean_strings(soil)
-    microclimate = clean_strings(microclimate)
-    soil_microclimate = clean_strings(soil_microclimate)
+    if ARGV[4] == "1"
+      microclimate = clean_strings(microclimate)
+      soil_microclimate = clean_strings(soil_microclimate)
+    end
     metadata = clean_strings(metadata)
 
     #Create city_parcel table
-    merger = merge_cpp([iTree, veg_survey, veg_transect, lawn_survey, phone_survey, soil, microclimate, soil_microclimate, metadata])
-    iTree, veg_survey, veg_transect, lawn_survey, phone_survey, soil, microclimate, soil_microclimate, metadata = merger[0]
+    if ARGV[4] == "1"
+      merger = merge_cpp([iTree, veg_survey, veg_transect, lawn_survey, phone_survey, soil, microclimate, soil_microclimate, metadata])
+      iTree, veg_survey, veg_transect, lawn_survey, phone_survey, soil, microclimate, soil_microclimate, metadata = merger[0]
+    else
+      merger = merge_cpp([iTree, veg_survey, veg_transect, lawn_survey, phone_survey, soil, metadata])
+      iTree, veg_survey, veg_transect, lawn_survey, phone_survey, soil, metadata = merger[0]
+    end
     city_parcel = merger[1]
 
     #Cleanup site names
@@ -289,7 +298,7 @@ if ARGV.length == 3
     #Database writing#######
     ########################    
     puts "Writing database..."
-    db.execute "CREATE TABLE iTree (cpp_index TEXT, tree_no TEXT, sp_index TEXT, sp_common TEXT, dbh TEXT, height TEXT, ground_area TEXT, condition TEXT, leaf_area TEXT, leaf_biomass TEXT, leaf_area_index TEXT, carbon_storage TEXT, gross_carbon_seq TEXT, money_value TEXT, street TEXT, native TEXT)"
+    db.execute "CREATE TABLE iTree (cpp_index TEXT, tree_no TEXT, sp_index TEXT, sp_common TEXT, dbh TEXT, height TEXT, ground_area TEXT, condition TEXT, leaf_area TEXT, leaf_biomass TEXT, leaf_area_index TEXT, carbon_storage TEXT, gross_carbon_seq TEXT, money_value TEXT, street TEXT, native TEXT, parcel_area TEXT, impervious_parcel_area TEXT)"
     add_to_data_base(iTree, db, "iTree", nil)
     db.execute "CREATE TABLE VegSurvey (cpp_index TEXT, sp_common TEXT, sp_index TEXT, sp_native TEXT, location TEXT, cultivation TEXT, notes TEXT)"
     add_to_data_base(veg_survey, db, "VegSurvey", nil)
@@ -299,10 +308,12 @@ if ARGV.length == 3
     add_to_data_base(lawn_survey, db, "LawnSurvey", nil)
     db.execute "CREATE TABLE PhoneSurvey (cpp_index TEXT, income TEXT, landuse TEXT, year_built TEXT, yard_type TEXT, income_survey TEXT, income_combined TEXT, density_level TEXT, income_level TEXT, north_south TEXT, east_west TEXT, floral_biodiversity TEXT, local_nature_provisioning TEXT, supporting_environmental_services TEXT, local_cultural_value TEXT, neat_aesthetic TEXT, appearance TEXT, low_maintenance TEXT, low_cost TEXT, veg_private TEXT, veg_food TEXT, veg_cool TEXT, veg_condit TEXT, veg_common TEXT, veg_legac TEXT, yrd_weeds TEXT, yrd_looks TEXT, yrd_enjoy TEXT, yrd_social TEXT, yrd_common TEXT, yrd_air TEXT, yrd_climate TEXT, yrd_green TEXT, veg_aest TEXT, veg_ease TEXT, veg_wldlf TEXT, veg_nativ TEXT, veg_neat TEXT, veg_cost TEXT, veg_kids TEXT, veg_pets TEXT, veg_hoa TEXT, yrd_divers TEXT, yrd_learn TEXT, yrd_beaut TEXT, yrd_values TEXT, yrd_cost TEXT, yrd_tradit TEXT, yrd_drain TEXT, yrd_ease TEXT, yrd_pollut TEXT, yrd_neat TEXT, yrd_soil TEXT, yrd_kids TEXT, yrd_pets TEXT, yrd_flwrs TEXT)"   
     add_to_data_base(phone_survey, db, "PhoneSurvey", nil)
-    db.execute "CREATE TABLE Microclimate (cpp_index TEXT, date TEXT, time TEXT, type TEXT, value TEXT REAL)"
-    add_to_data_base(microclimate, db, "Microclimate", nil)
-    db.execute "CREATE TABLE SoilMicroclimate (cpp_index TEXT, date TEXT, time TEXT, temperature TEXT, moisture TEXT)"
-    add_to_data_base(soil_microclimate, db, "SoilMicroclimate", nil)
+    if ARGV[4] == "1"
+      db.execute "CREATE TABLE Microclimate (cpp_index TEXT, date TEXT, time TEXT, type TEXT, value TEXT REAL)"
+      add_to_data_base(microclimate, db, "Microclimate", nil)
+      db.execute "CREATE TABLE SoilMicroclimate (cpp_index TEXT, date TEXT, time TEXT, temperature TEXT, moisture TEXT)"
+      add_to_data_base(soil_microclimate, db, "SoilMicroclimate", nil)
+    end
     #Taxonomy requires special attention
     if (ARGV[2]=="1" or ARGV[2]=="2")
       db.execute "CREATE TABLE Taxonomy (sp_index TEXT, sp_binomial TEXT, sp_binomial_clean TEXT)"
@@ -334,4 +345,7 @@ else
   puts "\t\t0 - do nothing"
   puts "\t\t1 - build new taxonomy from scratch (requires R etc.)"
   puts "\t\t2 - use existing taxonomy lookup ('taxonomy.csv')"
+  puts "\tWhether to include microclimate data"
+  puts "\t\t0 - Skip"
+  puts "\t\t1 - Include (takes a while and has issues)"
 end
